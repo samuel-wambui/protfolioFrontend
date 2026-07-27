d14ed3a437a4c4865febb695bff7a56e50d73e69
samuel-wambui <samuelngari13@gmail.com>
2026-07-27 16:09:44 +0300

    Increase typography scale slightly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,30 +3,30 @@ import type { Config } from "tailwindcss";
 type FontSizeValue = [fontSize: string, configuration: { lineHeight: string }];
 
 const reducedFontSizes: Record<string, FontSizeValue> = {
-  xs: ["0.5625rem", { lineHeight: "0.75rem" }],
-  sm: ["0.65625rem", { lineHeight: "0.9375rem" }],
-  base: ["0.75rem", { lineHeight: "1.125rem" }],
-  lg: ["0.84375rem", { lineHeight: "1.3125rem" }],
-  xl: ["0.9375rem", { lineHeight: "1.3125rem" }],
-  "2xl": ["1.125rem", { lineHeight: "1.5rem" }],
-  "3xl": ["1.40625rem", { lineHeight: "1.6875rem" }],
-  "4xl": ["1.6875rem", { lineHeight: "1.875rem" }],
-  "5xl": ["2.25rem", { lineHeight: "1" }],
-  "6xl": ["2.8125rem", { lineHeight: "1" }],
-  "7xl": ["3.375rem", { lineHeight: "1" }],
-  "8xl": ["4.5rem", { lineHeight: "1" }],
-  "9xl": ["6rem", { lineHeight: "1" }],
+  xs: ["0.61875rem", { lineHeight: "0.825rem" }],
+  sm: ["0.721875rem", { lineHeight: "1.03125rem" }],
+  base: ["0.825rem", { lineHeight: "1.2375rem" }],
+  lg: ["0.928125rem", { lineHeight: "1.44375rem" }],
+  xl: ["1.03125rem", { lineHeight: "1.44375rem" }],
+  "2xl": ["1.2375rem", { lineHeight: "1.65rem" }],
+  "3xl": ["1.546875rem", { lineHeight: "1.85625rem" }],
+  "4xl": ["1.85625rem", { lineHeight: "2.0625rem" }],
+  "5xl": ["2.475rem", { lineHeight: "1" }],
+  "6xl": ["3.09375rem", { lineHeight: "1" }],
+  "7xl": ["3.7125rem", { lineHeight: "1" }],
+  "8xl": ["4.95rem", { lineHeight: "1" }],
+  "9xl": ["6.6rem", { lineHeight: "1" }],
 };
 
 const reducedLineHeights: Record<string, string> = {
-  3: "0.5625rem",
-  4: "0.75rem",
-  5: "0.9375rem",
-  6: "1.125rem",
-  7: "1.3125rem",
-  8: "1.5rem",
-  9: "1.6875rem",
-  10: "1.875rem",
+  3: "0.61875rem",
+  4: "0.825rem",
+  5: "1.03125rem",
+  6: "1.2375rem",
+  7: "1.44375rem",
+  8: "1.65rem",
+  9: "1.85625rem",
+  10: "2.0625rem",
 };
 
 const config: Config = {
